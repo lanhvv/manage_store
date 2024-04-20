493769d9b8b1c99d4c1eaae525f006e42778e2e1
lanhvv1 <vuvanlanh10@gmail.com>
2024-04-20 21:45:28 +0700

    admin - user

--- src/app/admin/user/user.component.ts
+++ src/app/admin/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { Enums } from 'src/app/shared/Enums.service';
+import { UserService } from 'src/app/shared/service/user.service';
+import { Util } from 'src/app/shared/util.service';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  Util = Util;
+  Enums = Enums;
+  users: any[] = [];
+  title: string = "";
 
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {
+    // of(this.route.routeConfig).subscribe((routeConfig: any) => {
+      debugger
+      const role = this.route.snapshot.routeConfig?.path == Enums.CUSTOMERS ? Enums.CUSTOMER : Enums.EMPLOYEE;
+      this.title = role == Enums.CUSTOMER ? Enums.CUSTOMER_TITLE : Enums.EMPLOYEE_TITLE;
+      this.users = this.userService.getUsers().filter((x: any) => role == x?.role);
+    // });
+  }
 }
